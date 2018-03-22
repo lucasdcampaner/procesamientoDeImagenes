76968b58e3dac1ad4400a8ae62599f4466ab53bb
Lucas <lucasdcampaner@gmail.com>
2018-03-22 12:21:37 -0300

    Agrega el proyecto base.

---
+++ TP0/Demo/src/main/java/ar/edu/untref/imagenes/Program.java
@@ -0,0 +1,9 @@
+package ar.edu.untref.imagenes;
+
+public class Program 
+{
+    public static final void main(String arg[])
+    {
+        
+    }
+}
---
+++ TP0/Demo/src/test/java/ar/edu/untref/imagenes/IntegracionTest.java
@@ -0,0 +1,21 @@
+package ar.edu.untref.imagenes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntegracionTest {
+	
+	@Test
+	public void test() {
+		
+		
+		//A
+		
+		//A
+		
+		Assert.assertTrue(true);
+		
+	}
+
+	
+}
