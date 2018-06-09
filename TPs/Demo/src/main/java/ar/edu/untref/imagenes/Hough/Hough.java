addc76597bd0441cea08f7cef26f0a49f971e856
Lucas <lucasdcampaner@gmail.com>
2018-06-09 10:04:55 -0300

    Borra codigo comentado y corrige indentacion.

--- TPs/Demo/src/main/java/ar/edu/untref/imagenes/Hough/Hough.java
+++ TPs/Demo/src/main/java/ar/edu/untref/imagenes/Hough/Hough.java
@@ -9,34 +9,30 @@ import ij.ImagePlus;
 
 public class Hough {
 
-    public Hough() {
-
-    }
-
     private ImagePlus imageResult;
     private ImagePlus imageOriginal;
     private ImagePlus imageFiltered;
+
+    protected int StepsPerDegree = 1;
+    protected int Radius = 4;
+
+    public Hough() {
+    }
 
     public Hough(ImagePlus imageOriginal, ImagePlus imageFiltered) {
         this.imageOriginal = imageOriginal;
         this.imageFiltered = imageFiltered;
     }
 
-    protected int StepsPerDegree = 1;
-    protected int Radius = 4;
-
     public void setStepsPerDegree(int stepsPerDegree) {
         this.StepsPerDegree = stepsPerDegree;
     }
-    
+
     public void setRadius(int radius) {
         this.Radius = radius;
     }
-    
-    public ImagePlus deteccionDeRectas2() {
 
-        int height = imageOriginal.getWidth();
-        int width = imageOriginal.getHeight();
+    public ImagePlus deteccionDeRectas2() {
 
         imageResult = new ImagePlus();
         imageResult = imageFiltered;
@@ -70,30 +66,20 @@ public class Hough {
 
     public int[][] pasarPrewitt(int[][] matrixGray) {
 
-        // prewit se pasa primero:
+        final int DERIVATE_X = 0;
+        final int DERIVATE_Y = 1;
+
         BorderDetectors borderDetectors;
         Functions functions;
         functions = new Functions();
         borderDetectors = new BorderDetectors(functions);
 
-        final int DERIVATE_X = 0;
-        final int DERIVATE_Y = 1;
-
         int[][] matrixWeight = { { -1, 0, 1 }, { -1, 0, 1 }, { -1, 0, 1 } };
-
         int[][] matrixDXR = borderDetectors.applyBorderDetector(matrixGray, matrixWeight, DERIVATE_X);
-        // .applyBorderDetectorToImageColor(matrixGray, matrixWeight,
-        // DERIVATE_X).get(0);
-
         int[][] matrixDYR = borderDetectors.applyBorderDetector(matrixGray, matrixWeight, DERIVATE_Y);
-        // .applyBorderDetectorToImageColor(matrixGray, matrixWeight,
-        // DERIVATE_Y).get(0);
-
         int[][] matrixResultR = Modifiers.calculateGradient(matrixDXR, matrixDYR);
-
         int[][] normalizedMatrixR = functions.normalizeMatrix(matrixResultR);
 
         return normalizedMatrixR;
     }
-
 }
